dade5739a44585791f6569497b14f62321d17f0f
Ramon Brand <ramon2009@gmail.com>
2022-09-13 10:49:20 +0300

    Add fps

--- powder/src/lib.rs
+++ powder/src/lib.rs
@@ -1,4 +1,6 @@
-use femtovg::{renderer::OpenGl, Canvas, Color};
+use std::time::Instant;
+
+use femtovg::{renderer::OpenGl, Canvas, Color, FontId};
 use glutin::{
     event::{ElementState, Event, MouseButton, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -10,7 +12,7 @@ use nalgebra_glm::Vec2;
 pub type RenderLayerFn<T> = Box<dyn FnMut(&mut Canvas<OpenGl>, &mut Meta, &mut T) -> ()>;
 
 pub struct Powder<T> {
-    state: T,
+    pub state: T,
     event_loop: EventLoop<()>,
     context: ContextWrapper<PossiblyCurrent, Window>,
     canvas: Canvas<OpenGl>,
@@ -27,6 +29,7 @@ where
         // Build window context
         let context = ContextBuilder::new()
             .with_multisampling(8)
+            .with_vsync(true)
             .build_windowed(window_builder, &event_loop);
         if context.is_err() {
             return Err("Could not build window".to_string());
@@ -64,8 +67,14 @@ where
         })
     }
     pub fn start(mut self) {
+        // Framerate management
+        let mut frame_timer = Instant::now();
+        let mut frame_counter = 0;
+        let mut frames_per_second = 0;
+
         let mut mouse_position = Vec2::new(0.0, 0.0);
         let mut is_mouse_down = false;
+        let mut is_mouse_released = false;
 
         self.event_loop.run(move |event, _, control_flow| {
             *control_flow = ControlFlow::Poll;
@@ -99,6 +108,7 @@ where
                         ElementState::Released => match button {
                             MouseButton::Left => {
                                 is_mouse_down = false;
+                                is_mouse_released = true;
                             }
                             _ => (),
                         },
@@ -107,6 +117,14 @@ where
                 },
                 Event::RedrawRequested(_) => {
                     // Before Rendering
+                    frame_counter = frame_counter + 1;
+                    let time_since_frame_counter_reset = Instant::now() - frame_timer;
+                    if time_since_frame_counter_reset.as_millis() >= 1000 {
+                        frame_timer = Instant::now();
+                        frames_per_second = frame_counter;
+                        frame_counter = 0;
+                    }
+
                     let size = window.inner_size();
                     self.canvas
                         .set_size(size.width, size.height, window.scale_factor() as f32);
@@ -120,8 +138,10 @@ where
 
                     // Construct state for current frame
                     let mut meta = Meta {
+                        frames_per_second,
                         mouse_position,
                         is_mouse_down,
+                        is_mouse_released,
                     };
 
                     // Rendering
@@ -132,6 +152,9 @@ where
                     // After Rendering
                     self.canvas.flush();
                     self.context.swap_buffers().expect("Could not swap buffers");
+
+                    // Reset Meta
+                    is_mouse_released = false;
                 }
                 Event::MainEventsCleared => window.request_redraw(),
                 _ => (),
@@ -141,9 +164,14 @@ where
     pub fn push(&mut self, render_layer: RenderLayerFn<T>) {
         self.render_layers.push(render_layer);
     }
+    pub fn load_font(&mut self, path: &str) -> FontId {
+        self.canvas.add_font(path).expect("Could not load font")
+    }
 }
 
 pub struct Meta {
+    pub frames_per_second: usize,
     pub mouse_position: Vec2,
     pub is_mouse_down: bool,
+    pub is_mouse_released: bool,
 }
